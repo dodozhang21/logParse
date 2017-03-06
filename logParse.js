694c0da00a7bc39ac810017ada97e81d4b1878fc
Ying Zhang <ying.zhang@meredith.com>
2017-03-05 18:13:34 -0800

    Remove unused function.

--- logParse.js
+++ logParse.js
@@ -186,11 +186,3 @@ function getRegex(line, regex) {
 
   return false;
 }
-
-function sortByKey(array, key) {
-  return array.sort(function (a, b) {
-    var x = a[key];
-    var y = b[key];
-    return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-  });
-}
